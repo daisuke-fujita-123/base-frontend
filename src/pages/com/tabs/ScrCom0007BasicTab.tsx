8e7ce0b6ee7c5233d8ea33ea9916ba87fc2ef674
ctc-hirano <tetsuya.hirano.307@ctc-g.co.jp>
2023-07-11 14:06:13 +0900

    SCR-COM-0007 実装完了（残対応あり

--- src/pages/com/tabs/ScrCom0007BasicTab.tsx
+++ src/pages/com/tabs/ScrCom0007BasicTab.tsx
@@ -1,23 +1,152 @@
-import { useState } from "react";
-import { useNavigate } from "hooks/useNavigate";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+
+import { MainLayout } from 'layouts/MainLayout';
+import { Section } from 'layouts/Section';
+
+import { DataGrid, GridColDef } from 'controls/Datagrid';
+
+import { getReport, ScrCom0007GetReportResponse } from 'apis/com/ScrCom0007Api';
+
+import { useNavigate } from 'hooks/useNavigate';
+
+/**
+ * 検索条件列定義
+ */
+const searchResultColumns: GridColDef[] = [
+    {
+        field: 'reportId',
+        headerName: '帳票ID',
+        size: 'm',
+        cellType: 'link',
+    },
+    {
+        field: 'reportName',
+        headerName: '帳票名',
+        size: 'm',
+    },
+    {
+        field: 'reportOutputFormatKind',
+        headerName: '帳票出力形式区分',
+        size: 'm',
+    },
+    {
+        field: 'commentEditFlag',
+        headerName: 'コメント編集フラグ',
+        size: 'm',
+    },
+    {
+        field: 'changeReservationFlag',
+        headerName: '変更予約フラグ',
+        size: 'm',
+    },
+    {
+        field: 'outputSourceFunctionName',
+        headerName: '出力元機能名',
+        size: 'm',
+    },
+];
+
+
+/**
+ * 検索結果行データモデル
+ */
+interface SearchResultRowModel {
+    // internalId
+    id: string;
+    // 帳票ID
+    reportId: string;
+    // 帳票名
+    reportName: string;
+    // 帳票出力形式区分
+    reportOutputFormatKind: string;
+    // コメント編集フラグ
+    commentEditFlag: boolean;
+    // 変更予約フラグ
+    changeReservationFlag: boolean;
+    // 出力元機能名
+    outputSourceFunctionName: string;
+}
+
+
+/**
+ * 帳票一覧情報取得APIレスポンスから検索結果モデルへの変換
+ */
+const convertToSearchResultRowModel = (
+    response: ScrCom0007GetReportResponse
+): SearchResultRowModel[] => {
+    return response.getReportSearchResult.map((x) => {
+        return {
+            id: x.reportId,
+            reportId: x.reportId,
+            reportName: x.reportName,
+            reportOutputFormatKind: x.reportOutputFormatKind,
+            commentEditFlag: x.commentEditFlag,
+            changeReservationFlag: x.changeReservationFlag,
+            outputSourceFunctionName: x.outputSourceFunctionName,
+        };
+    });
+};
+
 
 /**
  * SCR-COM-0007 帳票管理画面 基本情報タブ
  * @returns
  */
 const ScrCom0007BasicTab = () => {
+    // state
+    const [searchResult, setSearchResult] = useState<SearchResultRowModel[]>([]);
+    const [hrefs, setHrefs] = useState<any[]>([]);
+
     // router
-    const { corporationId } = useParams();
     const navigate = useNavigate();
 
-    // state
-    const [selectValues, setSelectValues] = useState<SelectValuesModel>(
-        selectValuesInitialValues
-    );
+    /**
+   * 初期表示
+   */
+    useEffect(() => {
+        const initialize = async () => {
+            const response = await getReport();
+            const searchResult = convertToSearchResultRowModel(response);
+            const hrefs = searchResult.map((x) => {
+                return {
+                    field: 'reportId',
+                    id: x.reportId,
+                    href: '/com/reports/' + x.reportId,
+                };
+            });
+            setSearchResult(searchResult);
+            setHrefs(hrefs);
+        };
+        initialize();
+    }, []);
+
+
+    /**
+  * リンククリック時のイベントハンドラ
+  */
+    const handleLinkClick = (url: string) => {
+        navigate(url);
+    };
+
 
     return (
         <>
+            <MainLayout>
+                {/* main */}
+                <MainLayout main>
+                    <Section name='帳票一覧'>
+                        {/* TODO: DataGridのセルの一部に下記条件分岐を追加 */}
+                        {/* １．コメント編集フラグがtrueの場合、<link>コンポーネント表示
+                            ２．コメント編集フラグがfalseの場合、<label>コンポーネント表示*/}
+                        <DataGrid
+                            columns={searchResultColumns}
+                            rows={searchResult}
+                            hrefs={hrefs}
+                            onLinkClick={handleLinkClick}
+                        />
+                    </Section>
+                </MainLayout>
+            </MainLayout>
         </>
     )
 }
--- src/pages/com/tabs/ScrCom0007ChangeHistoryTab.tsx
+++ src/pages/com/tabs/ScrCom0007ChangeHistoryTab.tsx
@@ -1,75 +1,181 @@
+import React, { useEffect, useState } from 'react';
+
+import { MarginBox } from 'layouts/Box';
+import { MainLayout } from 'layouts/MainLayout';
+import { Section } from 'layouts/Section';
+
+import { AddButton } from 'controls/Button';
+import { DataGrid, GridColDef } from 'controls/Datagrid';
+
+import { getChangeHistory, ScrCom0007GetChangeHistoryResponse } from 'apis/com/ScrCom0007Api';
+
+import { useNavigate } from 'hooks/useNavigate';
+
+/**
+ * 検索条件列定義
+ */
 const changeHistoryColumns: GridColDef[] = [
-    {
-        field: 'id',
-        headerName: '変更履歴番号',
-        cellType: 'link',
-    },
+  {
+    field: 'applicationId',
+    headerName: '申請ID',
+    size: 'm',
+    cellType: 'link',
+  },
+  {
+    field: 'applicationSourceScreen',
+    headerName: '申請元画面',
+    size: 'm',
+  },
+  {
+    field: 'tabAllRegist',
+    headerName: 'タブ名/一括登録',
+    size: 'm',
+  },
+  {
+    field: 'changeDate',
+    headerName: '変更日',
+    size: 'm',
+  },
+  {
+    field: 'applicantIdName',
+    headerName: '申請者ID/申請者名',
+    size: 'm',
+  },
+  {
+    field: 'applicantDateTime',
+    headerName: '申請日時',
+    size: 'm',
+  },
+  {
+    field: 'registUpdateMemoExistence',
+    headerName: '登録・変更メモ',
+    size: 'm',
+  },
 ];
 
+
+/**
+ * 検索結果行データモデル
+ */
 interface changeHistoryRowModel {
-    id: string;
+  // internalId
+  id: string;
+  // 申請ID
+  applicationId: string;
+  // 申請元画面
+  applicationSourceScreen: string;
+  // タブ名/一括登録
+  tabAllRegist: string;
+  // 変更日
+  changeDate: string;
+  // 申請者ID/申請者名
+  applicantIdName: string;
+  // 申請日時
+  applicantDateTime: string;
+  // 登録・変更メモ
+  registUpdateMemo: string;
+  // 登録・変更メモ有無
+  registUpdateMemoExistence: string;
+  // 帳票ID
+  reportId: string;
 }
 
-const chnageHistoryData: changeHistoryRowModel[] = [
-    {
-        id: '00000001',
-    },
-    {
-        id: '00000002',
-    },
-    {
-        id: '00000003',
-    },
-    {
-        id: '00000004',
-    },
-];
-
-const hrefs = [
-    {
-        field: 'id',
-        id: '00000001',
-        href: '/mem/corporations/CORP-1?change-history-number=00000001#basic',
-    },
-    {
-        field: 'id',
-        id: '00000002',
-        href: '/mem/corporations/CORP-1?change-history-number=00000002#credit',
-    },
-    {
-        field: 'id',
-        id: '00000003',
-        href: '/mem/corporations/CORP-1?change-history-number=00000003#credit-limit',
-    },
-    {
-        field: 'id',
-        id: '00000004',
-        href: '/mem/corporations/CORP-1?change-history-number=00000004#contract',
-    },
-];
-
-const ScrCom0007ChangeHistoryTab = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    const handleLinkClick = (url: string) => {
-        navigate(url, true);
+/**
+ * 変更履歴一覧情報取得APIレスポンスから検索結果モデルへの変換
+ */
+const convertToSearchResultRowModel = (
+  response: ScrCom0007GetChangeHistoryResponse
+): changeHistoryRowModel[] => {
+  return response.getChangeHistorySearchResult.map((x) => {
+    return {
+      id: x.applicationId,
+      applicationId: x.applicationId,
+      applicationSourceScreen: x.applicationSourceScreen,
+      tabAllRegist: x.tabAllRegist,
+      changeDate: x.changeDate,
+      applicantIdName: x.applicantIdName,
+      applicantDateTime: x.applicantDateTime,
+      registUpdateMemo: x.registUpdateMemo,
+      registUpdateMemoExistence: x.registUpdateMemoExistence,
+      reportId: x.reportId,
     };
+  });
+};
 
-    return (
-        <MainLayout>
-            <MainLayout main>
-                <Section name='変更履歴'>
-                    <DataGrid
-                        columns={changeHistoryColumns}
-                        rows={chnageHistoryData}
-                        hrefs={hrefs}
-                        onLinkClick={handleLinkClick}
-                    />
-                </Section>
-            </MainLayout>
-        </MainLayout>
-    );
+/**
+ * SCR-COM-0007 帳票管理画面 変更履歴タブ
+ * @returns
+ */
+const ScrCom0007ChangeHistoryTab = () => {
+  // state
+  const [searchResult, setSearchResult] = useState<changeHistoryRowModel[]>([]);
+  const [hrefs, setHrefs] = useState<any[]>([]);
+
+  // router
+  const navigate = useNavigate();
+
+  /**
+ * 初期表示
+ */
+  useEffect(() => {
+    const initialize = async () => {
+      const response = await getChangeHistory();
+      const searchResult = convertToSearchResultRowModel(response);
+      const hrefs = searchResult.map((x) => {
+        return {
+          field: 'applicationId',
+          id: x.applicationId,
+          href: '/com/reports/' + x.applicationId,
+        };
+      });
+      setSearchResult(searchResult);
+      setHrefs(hrefs);
+    };
+    initialize();
+  }, []);
+
+
+  /**
+   * 
+   * @param url 申請IDリンク押下時のイベントハンドラ
+   */
+  const handleLinkClick = (url: string) => {
+    navigate(url);
+  };
+
+  /**
+* CSV出力アイコンクリック時のイベントハンドラ
+*/
+  const handleIconOutputCsvClick = () => {
+    // TODO：CSV機能実装後に変更
+    alert('TODO:結果結果からCSVを出力する。');
+  };
+
+  return (
+    <MainLayout>
+      <MainLayout main>
+        <Section
+          name='変更履歴一覧'
+          decoration={
+            <MarginBox mt={2} mb={2} ml={2} mr={2} gap={2}>
+              {/* TODO：エクスポートアイコンに将来的に変更 */}
+              <AddButton onClick={handleIconOutputCsvClick}>
+                CSV出力
+              </AddButton>
+            </MarginBox>
+          }
+        >
+          {/* TODO: ツールチップで登録・変更メモを表示する */}
+          <DataGrid
+            columns={changeHistoryColumns}
+            rows={searchResult}
+            hrefs={hrefs}
+            onLinkClick={handleLinkClick}
+          />
+        </Section>
+      </MainLayout>
+    </MainLayout>
+  );
 };
 
 export default ScrCom0007ChangeHistoryTab;
