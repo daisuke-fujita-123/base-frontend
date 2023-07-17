e9b5d69476a8925de297a6c3be5e16760f0e249a
goshi-yoshizato-290 <goshi.yoshizato.290@ctc-g.co.jp>
2023-07-18 01:14:41 +0900

    SCR-MEM-0003 法人情報詳細 拠点枝番紐付けタブ 新規コミット

--- src/pages/mem/ScrMem0003Page.tsx
+++ src/pages/mem/ScrMem0003Page.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import ScrMem0003BranchNumberTab from 'pages/mem/tabs/ScrMem0003BranchNumberTab';
+import ScrMem0003ChangeHistoryTab from 'pages/mem/tabs/ScrMem0003ChangeHistoryTab';
+
+import { TabDef, Tabs } from 'layouts/Tabs';
 
 /**
- * SCR-MEM-0003 法人情報詳細画面
+ * 法人情報詳細画面
  */
 const ScrMem0003Page = () => {
+  // router
+  const location = useLocation();
+
+  const tabValues: TabDef[] = [
+    /*
+    { title: '基本情報', hash: '#basic' },
+    { title: '与信情報', hash: '#credit' },
+    { title: '与信制限', hash: '#credit-limit' },
+    { title: '契約情報', hash: '#contract' },
+    { title: '拠点情報', hash: '#base' },
+    { title: '取引履歴', hash: '#deal-history' },
+    */
+    { title: '拠点枝番紐付け', hash: '#branch-number' },
+    { title: '変更履歴', hash: '#change-hisotry' },
+  ];
+
   return (
-    <h1>SCR-MEM-0003 法人情報詳細</h1>
+    <Tabs tabDef={tabValues} defaultValue={location.hash}>
+      {/*
+      <ScrMem0003BasicTab />
+      <ScrMem0003CreditTab />
+      <ScrMem0003CreditLimitTab />
+      <ScrMem0003ContractTab />
+      <ScrMem0003BaseTab />
+      <ScrMem0003DealHistoryTab />
+      */}
+      <ScrMem0003BranchNumberTab />
+      <ScrMem0003ChangeHistoryTab />
+    </Tabs>
   );
 };
 
---
+++ src/providers/Message.ts
@@ -0,0 +1,18 @@
+export const messages = [
+  {
+    id: 'MSG-0001',
+    message: 'Loading....',
+  },
+  {
+    id: 'MSG-0002',
+    message: 'System Error :(',
+  },
+  {
+    id: 'MSG-0003',
+    message: '変更内容がすべて破棄されますがよろしいでしょうか。',
+  },
+  {
+    id: 'MSG-FR-ERR-00053',
+    message: '画面入力内容が編集されていません。',
+  },
+];
