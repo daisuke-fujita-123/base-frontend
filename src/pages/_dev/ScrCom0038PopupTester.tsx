ed938bed1ba2cdc2cf9d1efd46b5f1e179e8ffcb
ctc-hirano <tetsuya.hirano.307@ctc-g.co.jp>
2023-07-31 17:05:07 +0900

    共通部分の修正漏れ

--- src/pages/_dev/ScrCom0038PopupTester.tsx
+++ src/pages/_dev/ScrCom0038PopupTester.tsx
@@ -6,8 +6,7 @@ import ScrCom0038Popup from 'pages/com/popups/ScrCom0038Popup';
  * SCR-COM-0038 エラー確認（ポップアップ）
  */
 const ScrCom0038PopupTester = () => {
-  // return <ScrCxom0038Popup />;
-  return <></>;
+  return <ScrCxom0038Popup />;
 };
 
 export default ScrCom0038PopupTester;
