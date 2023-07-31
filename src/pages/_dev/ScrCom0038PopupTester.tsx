a3ce6ca9bd4a144f49d7b9c0ae2d610d42e7a1c7
ctc-hirano <tetsuya.hirano.307@ctc-g.co.jp>
2023-07-31 16:15:34 +0900

    エラー内容確認ポップアップ実装

--- src/pages/_dev/ScrCom0038PopupTester.tsx
+++ src/pages/_dev/ScrCom0038PopupTester.tsx
@@ -6,7 +6,8 @@ import ScrCom0038Popup from 'pages/com/popups/ScrCom0038Popup';
  * SCR-COM-0038 エラー確認（ポップアップ）
  */
 const ScrCom0038PopupTester = () => {
-  return <ScrCom0038Popup />;
+  // return <ScrCxom0038Popup />;
+  return <></>;
 };
 
 export default ScrCom0038PopupTester;
--- src/pages/com/popups/ScrCom0038Popup.tsx
+++ src/pages/com/popups/ScrCom0038Popup.tsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { FormProvider } from 'react-hook-form';
 
-import { yupResolver } from '@hookform/resolvers/yup';
-
 import { MainLayout } from 'layouts/MainLayout';
 import { Popup } from 'layouts/Popup';
 import { PopSection } from 'layouts/Section';
 
 import { CancelButton, ConfirmButton } from 'controls/Button';
 import { Checkbox } from 'controls/Checkbox';
+import { Typography } from 'controls/Typography';
 
 import { useForm } from 'hooks/useForm';
 import { useNavigate } from 'hooks/useNavigate';
-
-import { generate } from 'utils/BaseYup';
 
 import { SCREEN_ID } from 'definitions/screenId';
 
@@ -38,7 +35,7 @@ export interface errorList {
 }
 
 /**
- * ワーニング確認ポップアップ データモデル(ワーニング)
+ * エラー確認ポップアップ データモデル(ワーニング)
  */
 export interface warningList {
   // エラーメッセージ
@@ -52,47 +49,49 @@ interface ScrCom0038PopupProps {
   isOpen: boolean;
   data: ScrCom0038PopupModel;
   // キャンセルボタン押下時に渡すパラメータ
-  handleErrorPopupCancel: () => void;
+  handleCancel: () => void;
 }
-
-/**
- * TODO: バリデーションスキーマ
- */
-const validationSchama = generate(['applicationComment']);
 
 /**
  * エラー確認ポップアップ
  */
 const ScrCom0038Popup = (props: ScrCom0038PopupProps) => {
   // props
-  const { isOpen, data } = props;
+  const { isOpen, data, handleCancel } = props;
 
   // form
   const methods = useForm({
     defaultValues: {
       checkbox: false,
     },
-    resolver: yupResolver(validationSchama),
-    context: false,
+    // context: false,
   });
   const { setValue, watch } = methods;
 
   // state
   // ワーニングチェックボックスを全てチェックしたかどうかを管理するフラグ
-  const [isWarningChecked, setIsWarningChecked] = useState<boolean>(false);
+  const [isWarningChecked, setIsWarningChecked] = useState<boolean>();
 
   // route
   const navigate = useNavigate();
 
-  // TODO: チェックボックス処理
+  // チェックボックス処理
   useEffect(() => {
     const subscription = watch((value, { name, type }) => {
-      // textの変更も監視対象のため、setValueでtextを変更した場合を無視しないと無弁ループする
-      if (name !== 'checkbox') return;
-      if (value.checkbox === undefined) return;
-      // TODO: 全てのワーニングのチェックボックス選択したら確定ボタンが活性化
-      value.checkbox;
-      setIsWarningChecked(true);
+      // 全てのチェックボックスの値を取得
+      Object.entries(value).map((x, key) => {
+        // 存在するチェックボックスの頭の一つが余分の為削除
+        if (key >= 1) {
+          // 値がtrue以外の場合は未選択状態のチェックボックスあり
+          if (!x[1]) {
+            // 確定ボタンの非活性化
+            setIsWarningChecked(false);
+          } else {
+            // 確定ボタンの活性化
+            setIsWarningChecked(true);
+          }
+        }
+      });
     });
     return () => subscription.unsubscribe();
   }, [setValue, watch]);
@@ -102,13 +101,13 @@ const ScrCom0038Popup = (props: ScrCom0038PopupProps) => {
    */
   const handleConfirm = () => {
     // 遷移元画面が入金詳細 => 入金一覧画面へ遷移
-    if (props.data.expirationScreenId === SCREEN_ID[0].screenId) {
+    if (data.expirationScreenId === SCREEN_ID[0].screenId) {
       navigate('/tra/recepts');
       // 遷移元画面が計算書詳細 => 計算書一覧画面へ遷移
-    } else if (props.data.expirationScreenId === SCREEN_ID[1].screenId) {
+    } else if (data.expirationScreenId === SCREEN_ID[1].screenId) {
       navigate('/tra/statements');
       // 遷移元画面が到着一括入力画面 => 到着一括入力画面へ遷移
-    } else if (props.data.expirationScreenId === SCREEN_ID[2].screenId) {
+    } else if (data.expirationScreenId === SCREEN_ID[2].screenId) {
       navigate('/doc/arrives');
     }
   };
@@ -119,62 +118,89 @@ const ScrCom0038Popup = (props: ScrCom0038PopupProps) => {
         <MainLayout main>
           <FormProvider {...methods}>
             <Popup open={isOpen}>
-              <PopSection name='エラー内容'>
+              <Popup main>
                 {/* エラー発生無の場合は非表示 */}
-                {data.errorList.length > 0
-                  ? data.errorList.map((errorValue: any, errorIndex: any) => {
-                      return (
-                        <>
-                          <Checkbox
-                            name='checkbox'
-                            label={
-                              'エラーメッセージ' +
-                              errorIndex +
-                              1 +
-                              ':' +
-                              errorValue.errorMessage
-                            }
-                            key={errorIndex}
-                          />
-                        </>
-                      );
-                    })
-                  : ''}
-              </PopSection>
-              <PopSection name='ワーニング内容'>
+                {data.errorList.length > 0 ? (
+                  <PopSection name='エラー内容' isError>
+                    {data.errorList.length > 0
+                      ? data.errorList.map(
+                          (errorList: errorList, errorIndex: number) => {
+                            return (
+                              <>
+                                <Typography
+                                  key={
+                                    errorList.errorMessage +
+                                    String(errorIndex + 1)
+                                  }
+                                  variant='h6'
+                                >
+                                  {'エラーメッセージ' +
+                                    Number(errorIndex + 1) +
+                                    ':' +
+                                    errorList.errorMessage}
+                                </Typography>
+                              </>
+                            );
+                          }
+                        )
+                      : ''}
+                  </PopSection>
+                ) : (
+                  ''
+                )}
                 {/* ワーニング発生無の場合は非表示 */}
-                {data.warningList.length > 0
-                  ? data.warningList.map(
-                      (warningValue: any, warningIndex: any) => {
-                        return (
-                          <>
-                            {/* エラー発生有の場合はチェックボックス非活性 */}
-                            <Checkbox
-                              name='checkbox'
-                              label={
-                                'ワーニングメッセージ' +
-                                warningIndex +
-                                1 +
-                                ':' +
-                                warningValue.warningMessage
-                              }
-                              key={warningIndex}
-                              disabled={
-                                data.errorList.length > 0 ? true : false
-                              }
-                            />
-                          </>
-                        );
-                      }
-                    )
-                  : ''}
-              </PopSection>
-            </Popup>
-            <Popup bottom>
-              <CancelButton onClick={props.handleErrorPopupCancel}>
-                キャンセル
-              </CancelButton>
-              <ConfirmButton onClick={handleConfirm}>確定</ConfirmButton>
+                {data.warningList.length > 0 ? (
+                  <PopSection name='ワーニング内容' isWarning>
+                    {data.warningList.length > 0
+                      ? data.warningList.map(
+                          (warningList: warningList, warningIndex: number) => {
+                            return (
+                              <>
+                                {/* エラー発生有の場合はチェックボックス非活性 */}
+                                <Checkbox
+                                  key={
+                                    warningList.warningMessage +
+                                    String(warningIndex)
+                                  }
+                                  name={'checkbox' + warningIndex}
+                                  label={
+                                    'ワーニングメッセージ' +
+                                    Number(warningIndex + 1) +
+                                    ':' +
+                                    warningList.warningMessage
+                                  }
+                                  disabled={
+                                    data.errorList.length > 0 ? true : false
+                                  }
+                                />
+                              </>
+                            );
+                          }
+                        )
+                      : ''}
+                  </PopSection>
+                ) : (
+                  ''
+                )}
+              </Popup>
+              <Popup bottom>
+                <CancelButton onClick={handleCancel}>キャンセル</CancelButton>
+                <ConfirmButton
+                  onClick={handleConfirm}
+                  // 条件１：エラーが1件以上存在する場合 -> 非活性
+                  // 条件２：ワーニングが0件の場合 -> 活性
+                  // 条件３：ワーニングが1件以上存在し、全てのチェックボックスを選択済みの場合 -> 活性
+                  disable={
+                    data.errorList.length >= 1
+                      ? true
+                      : isWarningChecked
+                      ? false
+                      : true
+                  }
+                >
+                  確定
+                </ConfirmButton>
+              </Popup>
             </Popup>
           </FormProvider>
         </MainLayout>
