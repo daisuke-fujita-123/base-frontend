36093436e89cbc300681a67e8cd1445b0cdc676a
ctc-hirano <tetsuya.hirano.307@ctc-g.co.jp>
2023-07-31 17:06:51 +0900

    修正

--- src/pages/_dev/ScrCom0038PopupTester.tsx
+++ src/pages/_dev/ScrCom0038PopupTester.tsx
@@ -6,7 +6,7 @@ import ScrCom0038Popup from 'pages/com/popups/ScrCom0038Popup';
  * SCR-COM-0038 エラー確認（ポップアップ）
  */
 const ScrCom0038PopupTester = () => {
-  return <ScrCxom0038Popup />;
+  return <ScrCom0038Popup />;
 };
 
 export default ScrCom0038PopupTester;
