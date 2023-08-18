825aefa5547f081e2488757ee964bab6248b5cac
ctc-hirano <tetsuya.hirano.307@ctc-g.co.jp>
2023-08-18 10:38:00 +0900

    レビュー指摘対応(残有)

--- src/pages/com/ScrCom0023Page.tsx
+++ src/pages/com/ScrCom0023Page.tsx
@@ -1,41 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
+import { MarginBox } from 'layouts/Box';
 import { MainLayout } from 'layouts/MainLayout';
 import { Section } from 'layouts/Section';
-import { MarginBox } from 'layouts/Box';
-import { DataGrid, GridColDef } from 'controls/Datagrid';
+
 import { AddButton } from 'controls/Button';
+import {
+  DataGrid,
+  exportCsv,
+  GridColDef,
+  GridHrefsModel,
+} from 'controls/Datagrid';
+
+import { getPlace, ScrCom0023GetPlaceResponse } from 'apis/com/ScrCom0023Api';
+
 import { useNavigate } from 'hooks/useNavigate';
-import { ScrCom0023GetPlaceRequest, ScrCom0023GetPlaceResponse, getPlace } from 'apis/com/ScrCom0023Api';
-import { AppContext } from 'providers/AppContextProvider';
 
-
-/**
-* 検索条件データモデル
-*/
-interface SearchConditionModel {
-  // 業務日付
-  businessDate: string;
-  // ソートキー
-  sortKey: string;
-  // ソート方向
-  sortDirection: string;
-  // リミット
-  limit: number;
-  // オフセット
-  offset: number;
-}
-
-/**
- * 取得用検索条件初期データ
- */
-const initialValues: SearchConditionModel = {
-  // TODO:業務日付取得方法実装待ち
-  businessDate: '',
-  sortKey: 'placeCd',
-  sortDirection: 'asc',
-  limit: 100,
-  offset: 0,
-};
+import { AuthContext } from 'providers/AuthProvider';
 
 /**
  * 検索条件列定義
@@ -44,53 +25,53 @@ const searchResultColumns: GridColDef[] = [
   {
     field: 'placeCd',
     headerName: '会場コード',
-    width: 128,
     cellType: 'link',
+    size: 'm',
   },
   {
     field: 'placeName',
     headerName: '会場名',
-    width: 128,
+    size: 'l',
   },
   {
     field: 'sessionWeekKind',
     headerName: '開催曜日',
-    width: 128,
+    size: 's',
   },
   {
     field: 'omatomePlaceFlag',
     headerName: 'おまとめ会場',
-    width: 128,
+    size: 's',
   },
   {
     field: 'corporationId',
     headerName: '法人ID',
-    width: 128,
+    size: 'm',
   },
   {
     field: 'corporationName',
     headerName: '法人名',
-    width: 128,
+    size: 'l',
   },
   {
     field: 'contractId',
     headerName: '契約ID',
-    width: 128,
+    size: 'm',
   },
   {
     field: 'placeGroup',
     headerName: '会場グループ',
-    width: 128,
+    size: 'm',
   },
   {
     field: 'destinationPlace',
     headerName: '支払先会場名',
-    width: 128,
+    size: 'l',
   },
   {
     field: 'useFlag',
     headerName: '利用フラグ',
-    width: 128,
+    size: 's',
   },
 ];
 
@@ -107,7 +88,7 @@ interface SearchResultRowModel {
   // 開催曜日区分
   sessionWeekKind: string;
   // おまとめ会場フラグ
-  omatomePlaceFlag: boolean;
+  omatomePlaceFlag: string;
   // 法人ID
   corporationId: string;
   // 法人名
@@ -119,85 +100,98 @@ interface SearchResultRowModel {
   // 支払先会場
   destinationPlace: string;
   // 利用フラグ
-  useFlag: boolean;
+  useFlag: string;
 }
-
-/**
- * 検索条件モデルからライブ会場一覧取得APIリクエストへの変換
- */
-const convertFromSearchConditionModel = (SearchCondition: SearchConditionModel): ScrCom0023GetPlaceRequest => {
-  return {
-    businessDate: SearchCondition.businessDate,
-    sortKey: SearchCondition.sortKey,
-    sortDirection: SearchCondition.sortDirection,
-    limit: SearchCondition.limit,
-    offset: SearchCondition.offset,
-  }
-}
-
-/**
- * ライブ会場一覧取得APIレスポンスから検索結果モデルへの変換
- */
-const convertToSearchResultRowModel = (
-  response: ScrCom0023GetPlaceResponse
-): SearchResultRowModel[] => {
-  return response.searchResult.map((x) => {
-    return {
-      id: x.placeCd,
-      placeCd: x.placeCd,
-      placeName: x.placeName,
-      sessionWeekKind: x.sessionWeekKind,
-      omatomePlaceFlag: x.omatomePlaceFlag,
-      corporationId: x.corporationId,
-      corporationName: x.corporationName,
-      contractId: x.contractId,
-      placeGroup: x.placeGroup,
-      destinationPlace: x.destinationPlace,
-      useFlag: x.useFlag,
-    };
-  });
-};
-
 
 /**
  * SCR-COM-0023 ライブ会場一覧画面
  */
 const ScrCom0023Page = () => {
-
   // state
   const [searchResult, setSearchResult] = useState<SearchResultRowModel[]>([]);
-  const [hrefs, setHrefs] = useState<any[]>([]);
+  const [hrefs, setHrefs] = useState<GridHrefsModel[]>([]);
 
   // router
   const navigate = useNavigate();
 
   // user情報(businessDateも併せて取得)
-  const { appContext } = useContext(AppContext);
+  const { user } = useContext(AuthContext);
+
+  /**
+   * ライブ会場一覧取得APIレスポンスから検索結果モデルへの変換
+   */
+  const convertToSearchResultRowModel = (
+    response: ScrCom0023GetPlaceResponse
+  ): SearchResultRowModel[] => {
+    return response.searchResult.map((x) => {
+      return {
+        id: x.placeCd,
+        placeCd: x.placeCd,
+        placeName: x.placeName,
+        // 1：日、2：月、3：火、4：水、5：木、6：金、7：土"
+        sessionWeekKind:
+          x.sessionWeekKind === '1'
+            ? '日'
+            : x.sessionWeekKind === '2'
+            ? '月'
+            : x.sessionWeekKind === '3'
+            ? '火'
+            : x.sessionWeekKind === '4'
+            ? '水'
+            : x.sessionWeekKind === '5'
+            ? '木'
+            : x.sessionWeekKind === '6'
+            ? '金'
+            : x.sessionWeekKind === '7'
+            ? '土'
+            : '',
+        // true => "対象" false => "対象外"
+        omatomePlaceFlag: x.omatomePlaceFlag === true ? '対象' : '対象外',
+        corporationId: x.corporationId,
+        corporationName: x.corporationName,
+        contractId: x.contractId,
+        placeGroup: x.placeGroup,
+        destinationPlace: x.destinationPlace,
+        // true => "可" false => "不可"
+        useFlag: x.useFlag === true ? '可' : '不可',
+      };
+    });
+  };
 
   /**
    * 初期画面表示時にライブ会場一覧検索処理を実行
    */
   useEffect(() => {
     const initialize = async () => {
-      const request = convertFromSearchConditionModel(initialValues);
+      const request = {
+        businessDate: user.taskDate,
+        sortKey: 'placeCd',
+        sortDirection: 'asc',
+        limit: 100,
+        offset: 0,
+      };
       const response = await getPlace(request);
       const searchResult = convertToSearchResultRowModel(response);
-      const hrefs = searchResult.map((x) => {
-        return {
-          field: 'placeCd',
+
+      // 画面にデータを設定
+      setSearchResult(searchResult);
+
+      // hrefsを設定
+      const hrefs: GridHrefsModel[] = [{ field: 'placeCd', hrefs: [] }];
+      searchResult.map((x) => {
+        hrefs[0].hrefs.push({
           id: x.placeCd,
           href: '/com/places/' + x.placeCd,
-        };
+        });
       });
-      setSearchResult(searchResult);
       setHrefs(hrefs);
     };
     initialize();
   }, []);
 
   /**
-    * リンククリック時のイベントハンドラ
-    */
+   * リンククリック時のイベントハンドラ
+   */
   const handleLinkClick = (url: string) => {
     navigate(url);
   };
@@ -206,18 +200,15 @@ const ScrCom0023Page = () => {
    * 追加アイコンクリック時のイベントハンドラ
    */
   const handleIconAddClick = () => {
-    // TODO：新規作成用URI決定後に変更
-    navigate('/com/places/');
+    navigate('/com/places/new');
   };
 
   /**
    * CSV出力アイコンクリック時のイベントハンドラ
    */
-  const handleIconOutputCsvClick = () => {
-    // TODO：CSV機能実装後に変更
-    alert('TODO:結果結果からCSVを出力する。');
+  const handleExportCsvClick = () => {
+    exportCsv(searchResult, 'ScrCom0023.csv');
   };
-
 
   return (
     <>
@@ -229,11 +220,18 @@ const ScrCom0023Page = () => {
             name='ライブ会場一覧'
             decoration={
               <MarginBox mt={2} mb={2} ml={2} mr={2} gap={2}>
-                {/* TODO：エクスポートアイコンに将来的に変更 */}
-                <AddButton onClick={handleIconOutputCsvClick}>
-                  CSV出力
+                <AddButton onClick={handleExportCsvClick}>CSV出力</AddButton>
+                {/* 編集権限なしの場合 非活性 */}
+                <AddButton
+                  onClick={handleIconAddClick}
+                  disable={
+                    !user.editPossibleScreenIdList.includes('SCR-COM-0023')
+                      ? true
+                      : false
+                  }
+                >
+                  追加
                 </AddButton>
-                <AddButton onClick={handleIconAddClick}>追加</AddButton>
               </MarginBox>
             }
           >
@@ -241,15 +239,18 @@ const ScrCom0023Page = () => {
               columns={searchResultColumns}
               rows={searchResult}
               hrefs={hrefs}
-              // TODO: ページング処理不要
-              pageSize={10}
               onLinkClick={handleLinkClick}
+              // TODO: 利用フラグがfalseの場合はレコードグレーアウト
+              getCellDisabled={(params) => {
+                if (params.usaFlag === false) return true;
+                return false;
+              }}
             />
           </Section>
         </MainLayout>
       </MainLayout>
     </>
-  )
+  );
 };
 
 export default ScrCom0023Page;
