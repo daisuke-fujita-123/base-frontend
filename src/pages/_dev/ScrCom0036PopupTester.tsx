f9f3a2da2c5881a304b48dad5f593c6c25d87320
Kagatani-ctc <ryuho.kagatani@ctc-g.co.jp>
2023-08-22 16:54:19 +0900

    SCR-COM-0036 一括登録エラー確認 初回作成_テスター

--- src/pages/_dev/ScrCom0036PopupTester.tsx
+++ src/pages/_dev/ScrCom0036PopupTester.tsx
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import ScrCom0036Popup from 'pages/com/popups/ScrCom0036Popup';
+import ScrCom0036Popup, {
+  ScrCom0036PopupModel,
+} from 'pages/com/popups/ScrCom0036Popup';
+
+import { ScrCom0036GetAllRegistrationWorkResult } from 'apis/com/ScrCom0036Api';
 
 /**
  * SCR-COM-0036 一括登録エラー確認（ポップアップ）
  */
 const ScrCom0036PopupTester = () => {
-  return <ScrCom0036Popup />;
+  // エラー・ワーニング内容の初期化
+  const [contents, setContents] = useState<ScrCom0036PopupModel>({
+    errors: [
+      {
+        columnName: '',
+        columnValue: '',
+        message: '',
+      },
+    ],
+    warnings: [
+      {
+        columnName: '',
+        columnValue: '',
+        message: '',
+      },
+    ],
+  });
+
+  // テスト用: ボタンを一旦エラー内容取得APIの呼出用に見立てて処理実施
+  const getAllRegistErrInfo = async () => {
+    const response = await ScrCom0036GetAllRegistrationWorkResult({
+      // 引数の値は仮のサンプル
+      allRegistrationId: 'BRG-COM-0001',
+      changeHistoryNumber: 1,
+      number: 1,
+    });
+    const errList: {
+      columnName: string;
+      columnValue: string;
+      message: string;
+    }[] = [];
+    const warnList: {
+      columnName: string;
+      columnValue: string;
+      message: string;
+    }[] = [];
+    if (response.errorList && response.warnList) {
+      // 項目名が画面側とAPIレスポンスで異なるため、詰めなおす。
+      response.errorList.forEach((e) => {
+        const err = {
+          columnName: e.columnName,
+          columnValue: e.columnValue,
+          message: e.columnMessage,
+        };
+        errList.push(err);
+      });
+      response.warnList.forEach((e) => {
+        // 項目名が画面側とAPIレスポンスで異なるため、詰めなおす。
+        const warn = {
+          columnName: e.columnName,
+          columnValue: e.columnValue,
+          message: e.columnMessage,
+        };
+        warnList.push(warn);
+      });
+    }
+    // ポップアップに表示するコンテンツを設定
+    setContents({
+      errors: errList,
+      warnings: warnList,
+    });
+  };
+
+  return (
+    <ScrCom0036Popup
+      isOpen={true}
+      setIsOpen={getAllRegistErrInfo}
+      contents={contents}
+    />
+  );
 };
 
 export default ScrCom0036PopupTester;
--- src/pages/com/popups/ScrCom0036Popup.tsx
+++ src/pages/com/popups/ScrCom0036Popup.tsx
@@ -1,10 +1,221 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FormProvider } from 'react-hook-form';
+
+import { BlankLayout } from 'layouts/InputLayout';
+import { Popup } from 'layouts/Popup';
+import { Stack } from 'layouts/Stack';
+import { StackSection } from 'layouts/StackSection';
+
+import { CancelButton } from 'controls/Button';
+import { DataGrid, GridColDef } from 'controls/Datagrid';
+
+import { useForm } from 'hooks/useForm';
+
+import { Box, Typography } from '@mui/material';
+
+/**
+ * 一括登録エラー確認ポップアップデータモデル
+ */
+export interface ScrCom0036PopupModel {
+  // エラー内容リスト配列
+  errors?: {
+    columnName: string;
+    columnValue: string;
+    message: string;
+  }[];
+  // ワーニング内容リスト配列
+  warnings?: {
+    columnName: string;
+    columnValue: string;
+    message: string;
+  }[];
+}
+
+/** エラー一覧 カラム定義 */
+const errorsColumns: GridColDef[] = [
+  {
+    field: 'no',
+    headerName: 'No.',
+    size: 'ss',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'columnName',
+    headerName: '項目',
+    size: 'm',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'columnValue',
+    headerName: '値',
+    size: 'm',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'message',
+    headerName: 'エラー内容',
+    size: 'l',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+];
+
+/** ワーニング一覧 カラム定義 */
+const warningsColumns: GridColDef[] = [
+  {
+    field: 'no',
+    headerName: 'No.',
+    size: 'ss',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'columnName',
+    headerName: '項目',
+    size: 'm',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'columnValue',
+    headerName: '値',
+    size: 'm',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+  {
+    field: 'message',
+    headerName: 'ワーニング内容',
+    size: 'l',
+    cellType: 'default',
+    editable: false,
+    filterable: false,
+  },
+];
+
+/**
+ * 一括登録エラー確認ポップアップのProps
+ */
+interface ScrCom0036PopupProps {
+  isOpen: boolean;
+  contents: ScrCom0036PopupModel;
+  setIsOpen: (isOpen: boolean) => void;
+}
 
 /**
  * SCR-COM-0036 一括登録エラー確認（ポップアップ）
  */
-const ScrCom0036Popup = () => {
-  return <h1>SCR-COM-0036 一括登録エラー確認（ポップアップ）</h1>;
+const ScrCom0036Popup = (props: ScrCom0036PopupProps) => {
+  // props
+  const { isOpen, setIsOpen, contents } = props;
+
+  // DataGrid：エラー一覧
+  const [errorsData, setErrorsData] = useState<{ [key: string]: string }[]>([]);
+  // DataGrid：ワーニング一覧
+  const [warningsData, setWarningsData] = useState<{ [key: string]: string }[]>(
+    []
+  );
+
+  // form
+  const methods = useForm<ScrCom0036PopupModel>({});
+
+  // 一括登録エラー確認ポップアップ表示時の処理
+  useEffect(() => {
+    const initialize = (contents: ScrCom0036PopupModel) => {
+      // エラーリストの設定
+      setErrorsData([]);
+      if (contents.errors) {
+        const errors = contents.errors.map<{
+          [key: string]: string;
+        }>((o, i) => {
+          const tmp: { [key: string]: string } = {};
+          tmp['id'] = i.toString();
+          tmp['no'] = (i + 1).toString();
+          for (const [key, value] of Object.entries(o)) {
+            tmp[key] = value;
+          }
+          return tmp;
+        });
+        setErrorsData(errors);
+      }
+
+      // ワーニングリストの設定
+      setWarningsData([]);
+      if (contents.warnings) {
+        const warnings = contents.warnings.map<{
+          [key: string]: string;
+        }>((o, i) => {
+          const tmp: { [key: string]: string } = {};
+          tmp['id'] = i.toString();
+          tmp['no'] = (i + 1).toString();
+          for (const [key, value] of Object.entries(o)) {
+            tmp[key] = value;
+          }
+          return tmp;
+        });
+        setWarningsData(warnings);
+      }
+    };
+    initialize(contents);
+  }, [contents, isOpen]);
+
+  // イベントハンドル：キャンセル
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Popup open={isOpen}>
+        <Popup main>
+          <StackSection titles={[{ name: 'エラー・ワーニング確認' }]}>
+            <Box>
+              <FormProvider {...methods}>
+                {errorsData.length !== 0 ? (
+                  <Stack>
+                    <Typography color={'#FF0000'}>
+                      下記項目が入力エラーとなっています。エラー内容を確認してください。
+                    </Typography>
+                    <DataGrid columns={errorsColumns} rows={errorsData} />
+                  </Stack>
+                ) : (
+                  <></>
+                )}
+                {errorsData.length !== 0 && warningsData.length !== 0 ? (
+                  <BlankLayout />
+                ) : (
+                  <></>
+                )}
+                {warningsData.length !== 0 ? (
+                  <Stack>
+                    <Typography>
+                      下記項目が警告となっています。ワーニング内容を確認してください。
+                    </Typography>
+                    <DataGrid columns={warningsColumns} rows={warningsData} />
+                  </Stack>
+                ) : (
+                  <></>
+                )}
+              </FormProvider>
+            </Box>
+          </StackSection>
+        </Popup>
+        <Popup bottom>
+          <CancelButton onClick={handleCancel}>キャンセル</CancelButton>
+        </Popup>
+      </Popup>
+    </>
+  );
 };
 
 export default ScrCom0036Popup;
