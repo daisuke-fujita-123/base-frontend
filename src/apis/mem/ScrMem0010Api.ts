003ecd8e4d4ea83d2635454c5f5f563bab5d5e64
kasumi-nakamura-ctc <kasumi.nakamura.414@ctc-g.co.jp>
2023-08-22 15:24:42 +0900

    API修正

--- src/apis/mem/ScrMem0010Api.ts
+++ src/apis/mem/ScrMem0010Api.ts
@@ -38,52 +38,16 @@ export interface ScrMem0010GetBusinessbaseResponse {
   bikeSalesStaff: string;
 }
 
-/** 事業拠点基本情報取得API */
+/** API-MEM-0010-0002: 事業拠点基本情報取得API */
 export const ScrMem0010GetBusinessbase = async (
   request: ScrMem0010GetBusinessbaseRequest
 ): Promise<ScrMem0010GetBusinessbaseResponse> => {
   const response = await memApiClient.post(
-    '/scr-mem-0010/get-businessbase',
+    '/api/mem/scr-mem-0010/get-businessbase',
     request
   );
   return response.data;
 };
-
-/** コード値取得API（コード管理マスタ以外）リクエスト */
-export interface ScrMem9999GetCodeValueRequest {
-  // エンティティリスト
-  entityList: EntityList[];
-}
-
-// エンティティリスト
-export interface EntityList {
-  // エンティティ名
-  entityName: string;
-}
-
-/** コード値取得API（コード管理マスタ以外）レスポンス */
-export interface ScrMem9999GetCodeValueResponse {
-  // 結果リスト
-  resultList: ResultList[];
-}
-
-// 結果リスト
-export interface ResultList {
-  // エンティティ名
-  entityName: string;
-  // コード値リスト
-  codeValueList: CodeValueList[];
-}
-
-// コード値リスト
-export interface CodeValueList {
-  // コード値
-  codeValue: string;
-  // コード値名称
-  codeValueName: string;
-  // コード値名称カナ
-  codeValueNameKana: string;
-}
 
 /** 事業拠点契約コース・サービス一覧取得APIリクエスト */
 export interface ScrMem0010GetContractRequest {
@@ -151,41 +115,12 @@ export interface ListResult {
   optionUseStartDate: string;
 }
 
-/** 事業拠点契約コース・サービス一覧取得API */
+/** API-MEM-0010-0003: 事業拠点契約コース・サービス一覧取得API */
 export const ScrMem0010GetContract = async (
   request: ScrMem0010GetContractRequest
 ): Promise<ScrMem0010GetContractResponse> => {
   const response = await memApiClient.post(
-    '/scr-mem-0010/get-contract',
-    request
-  );
-  return response.data;
-};
-
-/** 営業担当情報取得APIリクエスト */
-export interface ScrMem0010GetEmployeeRequest {
-  // 業務日付
-  businessDate: Date;
-}
-
-/** 営業担当情報取得APIレスポンス */
-export interface ScrMem0010GetEmployeeResponse {
-  tvaaContractInfo: ContractResult[];
-  bikeContractInfo: ContractResult[];
-}
-
-// リスト
-export interface ContractResult {
-  salesId: string;
-  salesName: string;
-}
-
-/** 営業担当情報取得API */
-export const ScrMem0010GetEmployee = async (
-  request: ScrMem0010GetEmployeeRequest
-): Promise<ScrMem0010GetEmployeeResponse> => {
-  const response = await memApiClient.post(
-    '/scr-mem-0010/get-employee',
+    '/api/mem/scr-mem-0010/get-contract',
     request
   );
   return response.data;
@@ -224,12 +159,12 @@ export interface errorResult {
   detail: string;
 }
 
-/** 事業拠点基本情報入力チェックAPI */
+/** API-MEM-0010-0005: 事業拠点基本情報入力チェックAPI */
 export const ScrMem0010InputCheckBusinessBase = async (
   request: ScrMem0010InputCheckBusinessBaseRequest
 ): Promise<ScrMem0010InputCheckBusinessBaseResponse> => {
   const response = await memApiClient.post(
-    '/scr-mem-0010/input-check-business-base',
+    '/api/mem/scr-mem-0010/input-check-business-base',
     request
   );
   return response.data;
@@ -285,12 +220,12 @@ export interface ScrMem0010RegistrationBusinessBaseRequest {
   tabId: string;
 }
 
-/** 事業拠点登録API */
+/** API-MEM-0010-0011: 事業拠点登録API */
 export const ScrMem0010RegistrationBusinessBase = async (
   request: ScrMem0010RegistrationBusinessBaseRequest
 ): Promise<null> => {
   const response = await memApiClient.post(
-    '/scr-mem-0010/registration-business-base',
+    '/api/mem/scr-mem-0010/registration-business-base',
     request
   );
   return null;
