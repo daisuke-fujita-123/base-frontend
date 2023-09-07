795a308386b670e1bbfda65edb2646eb2b89bc21
Kagatani-ctc <ryuho.kagatani@ctc-g.co.jp>
2023-09-07 11:17:52 +0900

    単体テスト実施後 3_PCL(キャンセル)ケース3-2 キャンセル処理修正(テスタ側のみ)

--- src/pages/_dev/ScrCom0036PopupTester.tsx
+++ src/pages/_dev/ScrCom0036PopupTester.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ScrCom0036Popup, {
   ScrCom0036PopupModel,
@@ -11,6 +11,7 @@ import { ScrCom0036GetAllRegistrationWorkResult } from 'apis/com/ScrCom0036Api';
  */
 const ScrCom0036PopupTester = () => {
   // エラー・ワーニング内容の初期化
+  const [isOpenPopup, setIsOpenPopup] = useState(true);
   const [contents, setContents] = useState<ScrCom0036PopupModel>({
     errors: [
       {
@@ -28,54 +29,61 @@ const ScrCom0036PopupTester = () => {
     ],
   });
 
-  // テスト用: ボタンを一旦エラー内容取得APIの呼出用に見立てて処理実施
-  const getAllRegistErrInfo = async () => {
-    const response = await ScrCom0036GetAllRegistrationWorkResult({
-      // 引数の値は仮のサンプル
-      allRegistrationId: 'BRG-COM-0001',
-      changeHistoryNumber: 1,
-      number: 1,
-    });
-    const errList: {
-      columnName: string;
-      columnValue: string;
-      message: string;
-    }[] = [];
-    const warnList: {
-      columnName: string;
-      columnValue: string;
-      message: string;
-    }[] = [];
-    if (response.errorList && response.warnList) {
-      // 項目名が画面側とAPIレスポンスで異なるため、詰めなおす。
-      response.errorList.forEach((e) => {
-        const err = {
-          columnName: e.columnName,
-          columnValue: e.columnValue,
-          message: e.columnMessage,
-        };
-        errList.push(err);
+  // 初期表示
+  useEffect(() => {
+    const initial = async () => {
+      const response = await ScrCom0036GetAllRegistrationWorkResult({
+        // 引数の値は仮のサンプル
+        allRegistrationId: 'BRG-COM-0001',
+        changeHistoryNumber: 1,
+        number: 1,
       });
-      response.warnList.forEach((e) => {
+      const errList: {
+        columnName: string;
+        columnValue: string;
+        message: string;
+      }[] = [];
+      const warnList: {
+        columnName: string;
+        columnValue: string;
+        message: string;
+      }[] = [];
+      if (response.errorList && response.warnList) {
         // 項目名が画面側とAPIレスポンスで異なるため、詰めなおす。
-        const warn = {
-          columnName: e.columnName,
-          columnValue: e.columnValue,
-          message: e.columnMessage,
-        };
-        warnList.push(warn);
+        response.errorList.forEach((e) => {
+          const err = {
+            columnName: e.columnName,
+            columnValue: e.columnValue,
+            message: e.columnMessage,
+          };
+          errList.push(err);
+        });
+        response.warnList.forEach((e) => {
+          // 項目名が画面側とAPIレスポンスで異なるため、詰めなおす。
+          const warn = {
+            columnName: e.columnName,
+            columnValue: e.columnValue,
+            message: e.columnMessage,
+          };
+          warnList.push(warn);
+        });
+      }
+      // ポップアップに表示するコンテンツを設定
+      setContents({
+        errors: errList,
+        warnings: warnList,
       });
-    }
-    // ポップアップに表示するコンテンツを設定
-    setContents({
-      errors: errList,
-      warnings: warnList,
-    });
+    };
+  }, []);
+
+  // テスト用: ボタンを一旦エラー内容取得APIの呼出用に見立てて処理実施
+  const getAllRegistErrInfo = () => {
+    setIsOpenPopup(false);
   };
 
   return (
     <ScrCom0036Popup
-      isOpen={true}
+      isOpen={isOpenPopup}
       setIsOpen={getAllRegistErrInfo}
       contents={contents}
     />
