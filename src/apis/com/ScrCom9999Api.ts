1e51653caa43afcf84d89d7f07e7536da73326fb
takuma-sato-756-ctc <takuma.sato.756@ctc-g.co.jp>
2023-09-19 09:24:09 +0900

    変数名修正（front-backend間の調整）

--- src/apis/com/ScrCom9999Api.ts
+++ src/apis/com/ScrCom9999Api.ts
@@ -9,7 +9,7 @@ export interface ScrCom9999GetParentorganizationidListboxRequest {
 /** API-COM-9999-0002: 親組織ID情報取得API レスポンス */
 export interface ScrCom9999GetParentorganizationidListboxResponse {
   // リスト
-  searchGetParentorganizationidListbox: SearchGetParentorganizationidListbox[];
+  parentorganizationidList: SearchGetParentorganizationidListbox[];
 }
 
 /** API-COM-9999-0002: 親組織ID情報取得API レスポンス(リスト行) */
@@ -40,7 +40,7 @@ export interface ScrCom9999GetOrganizationidListboxRequest {
 /** API-COM-9999-0003: 組織ID情報取得API レスポンス */
 export interface ScrCom9999GetOrganizationidListboxResponse {
   // リスト
-  searchGetOrganizationidListbox: SearchGetOrganizationidListbox[];
+  organizationidList: SearchGetOrganizationidListbox[];
 }
 
 /** API-COM-9999-0003: 組織ID情報取得API レスポンス(リスト行) */
@@ -65,7 +65,7 @@ export const ScrCom9999GetOrganizationidListbox = async (
 /** API-COM-9999-0004: 画面権限ID情報取得API レスポンス */
 export interface ScrCom9999GetScreenpermissionidListboxResponse {
   // リスト
-  searchGetScreenpermissionidListbox: SearchGetScreenpermissionidListbox[];
+  screenpermissionidList: SearchGetScreenpermissionidListbox[];
 }
 
 /** API-COM-9999-0004: 画面権限ID情報取得API レスポンス(リスト行) */
@@ -90,7 +90,7 @@ export const ScrCom9999GetScreenpermissionidListbox = async (
 /** API-COM-9999-0005: マスタ権限ID情報取得API レスポンス */
 export interface ScrCom9999GetMasterpermissionidResponse {
   // リスト
-  searchGetMasterpermissionidListbox: SearchGetMasterpermissionid[];
+  masterpermissionidList: SearchGetMasterpermissionid[];
 }
 
 /** API-COM-9999-0005: マスタ権限ID情報取得API レスポンス(リスト行) */
@@ -115,7 +115,7 @@ export const ScrCom9999GetMasterpermissionid = async (
 /** API-COM-9999-0006: 承認権限ID情報取得API レスポンス */
 export interface ScrCom9999GetApprovalPermissionIdResponse {
   // リスト
-  searchGetApprovalPermissionIdListbox: SearchGetApprovalPermissionIdListbox[];
+  approvalPermissionList: SearchGetApprovalPermissionIdListbox[];
 }
 
 /** API-COM-9999-0006: 承認権限ID情報取得API レスポンス(リスト行) */
@@ -140,7 +140,7 @@ export const ScrCom9999GetApprovalPermissionId = async (
 /** API-COM-9999-0007: 所属組織IDリストボックス情報取得API レスポンス */
 export interface ScrCom9999GetBelongOrganizationIdResponse {
   // リスト
-  searchGetBelongOrganizationIdListbox: SearchGetBelongOrganizationIdListbox[];
+  organizationList: SearchGetBelongOrganizationIdListbox[];
 }
 
 /** API-COM-9999-0007: 所属組織IDリストボックス情報取得API レスポンス(リスト行) */
@@ -165,7 +165,7 @@ export const ScrCom9999GetBelongOrganizationId = async (
 /** API-COM-9999-0008: 所属役職IDリストボックス情報取得API レスポンス */
 export interface ScrCom9999GetPostIdResponse {
   // リスト
-  searchGetPostIdListbox: SearchGetPostIdListbox[];
+  postList: SearchGetPostIdListbox[];
 }
 
 /** API-COM-9999-0008: 所属役職IDリストボックス情報取得API レスポンス(リスト行) */
